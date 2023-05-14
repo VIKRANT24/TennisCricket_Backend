23ab04464bd7e2c1bd2dc2134f4139245f60bd28
Saurabh <saurabhkadu8606@gmail.com>
2023-05-14 14:35:57 +0530

    initial commit

---
+++ app/config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  HOST: "tenniscricket.ct274hdg8ja8.eu-north-1.rds.amazonaws.com",
+  USER: "TennisAdmin",
+  PASSWORD: "TennisAdmin#123",
+  DB: "tenniscr_tenniscorer",
+  PORT: "3306"
+};
