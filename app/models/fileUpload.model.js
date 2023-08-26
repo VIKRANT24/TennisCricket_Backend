b4b361b7e779b41d80f96a553c029dca541e50ee
Saurabh <saurabhkadu8606@gmail.com>
2023-08-27 03:27:24 +0530

    add dynamic folder

--- app/models/fileUpload.model.js
+++ app/models/fileUpload.model.js
@@ -4,7 +4,7 @@ const maxSize = 2 * 1024 * 1024;
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, __basedir + "/resources/admin/");
+    cb(null, __basedir + "/resources/"+req.query.folder+"/");
   },
   filename: (req, file, cb) => {
     console.log(file);
