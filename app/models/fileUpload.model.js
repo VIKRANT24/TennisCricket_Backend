ebd5f63c07db34585d2055ea50cd70cc6ef233b9
Saurabh <saurabhkadu8606@gmail.com>
2023-08-27 02:53:52 +0530

    add resources

--- app/models/fileUpload.model.js
+++ app/models/fileUpload.model.js
@@ -4,7 +4,7 @@ const maxSize = 2 * 1024 * 1024;
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, __basedir + "/resources/demo/");
+    cb(null, __basedir + "/resources/admin/");
   },
   filename: (req, file, cb) => {
     console.log(file);
