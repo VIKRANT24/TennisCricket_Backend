fd799ff12ac347e887b05fd17c8f9573cd797aa8
Saurabh <saurabhkadu8606@gmail.com>
2023-08-27 01:50:56 +0530

    file upload

--- app/models/fileUpload.model.js
+++ app/models/fileUpload.model.js
@@ -7,7 +7,8 @@ let storage = multer.diskStorage({
     cb(null, __basedir + "/resources/demo/");
   },
   filename: (req, file, cb) => {
-    console.log(file);
+    console.log("file :"+file);
+    console.log("filename :"+filename);
     cb(null, "test");
   },
 });
